b397e15a4dd9bc78e3f2bdcf7eec8f7e2ae4fe81
Argir Argirov <dj.giro@gmail.com>
2017-10-18 11:17:13 -0400

    5 seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'apple', description: 'Your standard fruit.', price: 200)
+Product.create(name: 'banana', description: 'Goes bad fast, but protein!', price: 50)
+Product.create(name: 'orange', description: "Doesn't rhyme!", price: 70)
+Product.create(name: 'avocado', description: "Hipsters love these!", price: 190)
+Product.create(name: 'grapes', description: "You can make wine!", price: 260)
