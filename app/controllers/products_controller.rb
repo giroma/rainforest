380c8762557a1cba00ea7d88a43bb33a3cc79d68
Argir Argirov <dj.giro@gmail.com>
2017-10-18 11:43:34 -0400

    Show controller and view added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-  
+
   def create
 
   end
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
 
   end
   def show
-
+    @product = Product.find(params[:id])
   end
   def edit
 
