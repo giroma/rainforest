6a4c3539557447354945e1ae8eab153bd8e437f0
Demi Lee <demilee.to@gmail.com>
2017-10-18 12:16:53 -0400

    product#new defined

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,21 +4,37 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
 
+    if @product.save
+      redirect_to @product
+    else
+      render :new
+    end
   end
+
   def new
     @product = Product.new
   end
+
   def show
     @product = Product.find(params[:id])
   end
+
   def edit
 
   end
+
   def update
 
   end
+
   def destroy
 
   end
+end
+
+private
+def product_params
+  params.require(:product).permit(:name, :description, :price)
 end
