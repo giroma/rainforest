60a66bb07bd79e3dbf0622142fda056b49d7226b
Argir Argirov <dj.giro@gmail.com>
2017-10-18 16:00:55 -0400

    Review create controller implemented

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,24 @@
 class ReviewsController < ApplicationController
 
   def create
-  
+
+
+
+
+    @review = Review.new(
+    comment: params[:review][:comment],
+    product_id: params[:product_id]
+    )
+
+    if @review.save
+      # flash.notice = 'Saved succesfully'
+      redirect_to product_path(params[:product_id])
+    else
+      @product = Product.find(params[:product_id])
+      @reviews = @product.reviews
+      render "/products/show"
+      # render product_path(params[:product_id])
+    end
   end
 
   def edit
